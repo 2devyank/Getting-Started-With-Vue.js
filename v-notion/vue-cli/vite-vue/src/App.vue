9a0f5591cc78f7422cef20fe12653aa43bbbd259
dev <devyanknagpal2002@gmail.com>
2022-02-03 02:25:52 +0530

    CLI

---
+++ v-notion/vue-cli/vite-vue/src/App.vue
@@ -0,0 +1,50 @@
+<script >
+ import Modal from "./components/Modal.vue"
+export default{
+  data(){
+    return {
+      title:"devyank mera bachha ",
+      name:"devyank",
+      showcase:false
+    }
+  },
+  components:{
+Modal
+  },
+  methods:{
+    handleevent(){
+      console.log(this.$refs.name)
+    },
+    handleshow(){
+      this.showcase=!this.showcase
+    }
+  }
+}
+</script>
+
+<template>
+  <div @click="handleshow" >
+    Hello HelloWorld <br>
+    {{title}}
+    <input type="text " ref="name">
+    <button @click="handleevent">click me</button>
+    <div v-if="showcase">
+    <Modal  :name="name" @close="handleshow">
+      <h1>m Modal hu</h1>    
+{{name}}
+    </Modal>
+    </div>
+    <button >open component</button>
+    </div> 
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ v-notion/vue-cli/vite-vue/src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="com" @click="toggleshow">
+<slot></slot>
+</div>
+
+
+</template>
+
+<script>
+export default {
+props:['name'],
+methods:{
+    toggleshow(){
+        this.$emit('close')
+    }
+}
+}
+</script>
+<style >
+.com{
+    background-color: blue;
+    height: 300px;
+    width: 200px;
+}
+</style>
